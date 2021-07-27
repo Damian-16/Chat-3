ae538cf843ab4541bd42ef27da5f4c1b9d72f291
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-07-27 19:14:04 -0400

    se agrego la funcion en chatProvider para agregar y cargar mensajes

--- src/context/ChatProvider.js
+++ src/context/ChatProvider.js
@@ -12,6 +12,7 @@ export const ChatProvider = (props) => {
         email:null,
         estado:null,
     })
+    const [mensajes, setMensajes] = useState([])
     React.useEffect(()=>{
       detectarUsuario()
     } ,[])
@@ -20,6 +21,7 @@ export const ChatProvider = (props) => {
         auth.onAuthStateChanged(user=>{
             if(user){
                 setUsuario({uid:user.uid,email:user.email,estado:true})
+                cargarMensajes()
             }
             else{
                 setUsuario({uid:null,email:null,estado:false})
@@ -38,12 +40,30 @@ export const ChatProvider = (props) => {
     const cerrarSesion =()=>{
         auth.signOut()
     }
-    
+    const cargarMensajes =() =>{
+        db.collection('chat')
+        .onSnapshot(query =>{ //snapshot actualiza en tiempo real los mensajes
+           const arrayMensajes=  query.docs.map(item=>item.data())//nuestro recorrido
+           setMensajes(arrayMensajes)//esto empuja los mensajes al estado de mensajes
+        })
+    }
+    const agregarMensajes = async(uidChat,textoInput)=>{
+        try {
+            await db.collection('chat').add({
+                fecha:Date.now(),
+                texto:textoInput,
+                uid:uidChat  //no hace falta empujar los datos porq el onSnapshot lo actualiza en tiempo real
+
+            })
+        } catch (error) {
+            
+        }
+    }
 
    const saludo = 'hola desde chat'
 
     return (
-        <ChatContext.Provider value={{usuario,ingresoUsuario,cerrarSesion}}>
+        <ChatContext.Provider value={{usuario,ingresoUsuario,cerrarSesion,mensajes,agregarMensajes}}>
             {props.children}
         </ChatContext.Provider>
     )
